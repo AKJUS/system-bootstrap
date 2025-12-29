c1f177021917b4136efdbe18a34901bfc1831537
limpid-kzonix <balyszyn@gmail.com>
2025-12-29 21:33:37 +0200

    update alacritty theme

---
+++ .files/arch+hypr/arch-system-update.sh
@@ -0,0 +1,45 @@
+#!/bin/env zsh
+echo "Initializing user env..."
+source "$HOME/.zshrc"
+echo "\n"
+echo "## shell: $SHELL ##"
+echo "\n"
+echo "------------------------"
+echo "-  Updating system...  -"
+echo "------------------------"
+echo "System info: $(uname -a)"
+echo "\n"
+echo "Updating system via 'dnf'..."
+# Paru handles both official repos and AUR
+# -Syu: Sync, Refresh, Upgrade
+# --noconfirm: Equivalent to -y
+echo "Updating system via 'paru'..."
+paru -Syu --noconfirm
+
+flatpak update -y
+
+echo ""
+echo "Updating rust toolchain..."
+rustup update
+
+echo ""
+echo "Updating julia toolchain..."
+juliaup update
+
+echo ""
+echo "Updating SDKMAN..."
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk update
+sdk upgrade
+
+echo "Updating NVM..."
+nvm install node --reinstall-packages-from=current --latest-npm
+
+echo "Updating Miniforge3"
+mamba update --all
+
+echo "Updating astral.uv"
+uv self update
+
+echo "\nSystem maintenance: Cleaning pacman cache..."
+sudo pacman -Sc --noconfirm
--- scripts/arch/3.sh
+++ scripts/arch/3.sh
@@ -3,3 +3,7 @@
 sudo pacman -S nerd-fonts
 
 sudo pacman -Syu hyprlauncher hyprpaper hyprpicker hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprpwcenter hyprtoolkit hyprcursor  hyprland-guiutils
+
+sudo pacman -S fuse2
+
+paru appimagelauncher
