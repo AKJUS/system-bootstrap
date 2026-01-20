54b90dfc9f2f0e0427914f339f10cd70e1c3a0b6
limpid-kzonix <balyszyn@gmail.com>
2026-01-21 00:20:45 +0200

    feat(hypr): update the configuration on various services

--- scripts/arch/01-aur-packages.sh
+++ scripts/arch/01-aur-packages.sh
@@ -2,7 +2,7 @@ paru -S --noconfirm google-chrome ttf-nerd-fonts-symbols
 paru -S sddm-silent-theme-git
 paru -S --noconfirm sddm qt6-virtualkeyboard qt6-multimedia-ffmpeg
 # Probably not needed, as it's not that stable compared to GNOME
-paru -S --noconfirm sddm-themes-git
+paru -S --noconfirm sddm-themes-git easyeffects
 
 # Install GNOME whole set of packages (just for the sake of login manager)
 sudo pacman -Syu gdm
