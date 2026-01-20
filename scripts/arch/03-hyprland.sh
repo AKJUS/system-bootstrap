d3d5d36287a00a240fedc1ee655b1b7811b676b5
limpid-kzonix <balyszyn@gmail.com>
2026-01-21 00:04:35 +0200

    feat(hypr): setup hypr binding for brightness and audio controll, improve notifications, change vicinae theme

--- scripts/arch/03-hyprland.sh
+++ scripts/arch/03-hyprland.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
+#
+# sudo pacman -S nerd-fonts
+#
+# sudo pacman -Syu hyprlauncher hyprpaper hyprpicker hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprpwcenter hyprtoolkit hyprcursor  hyprland-guiutils
+#
+# sudo pacman -S fuse2
+#
+# paru -Syu appimagelauncher
 
-sudo pacman -S nerd-fonts
-
-sudo pacman -Syu hyprlauncher hyprpaper hyprpicker hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprpwcenter hyprtoolkit hyprcursor  hyprland-guiutils
-
-sudo pacman -S fuse2
-
-paru appimagelauncher
+paru -Syu dunst rofi swayosd
