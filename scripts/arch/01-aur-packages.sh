046e58a56adc53326fa448efc8ec21ee2793f2ee
limpid-kzonix <balyszyn@gmail.com>
2026-01-21 12:56:31 +0200

    feat(arch): btop theme, vicinae configs, aur packages, hyprland config

--- scripts/arch/01-aur-packages.sh
+++ scripts/arch/01-aur-packages.sh
@@ -3,6 +3,11 @@ paru -S sddm-silent-theme-git
 paru -S --noconfirm sddm qt6-virtualkeyboard qt6-multimedia-ffmpeg
 # Probably not needed, as it's not that stable compared to GNOME
 paru -S --noconfirm sddm-themes-git easyeffects
+paru -s --noconfirm wttrbar
+paru -S calf qjackctl lv2
 
 # Install GNOME whole set of packages (just for the sake of login manager)
 sudo pacman -Syu gdm
+
+sudo pacman -S lsp-plugins
+sudo pacman -S jack gstreamer gst-plugins-base
