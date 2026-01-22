5579bf611452a393af68fa6a538afc43f6fe3b9e
limpid-kzonix <balyszyn@gmail.com>
2026-01-22 23:54:35 +0200

    chore(hypr): update the waybar configs and minor improvements related to hypr setup

--- scripts/arch/01-aur-packages.sh
+++ scripts/arch/01-aur-packages.sh
@@ -1,8 +1,7 @@
-paru -S --noconfirm google-chrome ttf-nerd-fonts-symbols
-paru -S sddm-silent-theme-git
+paru -S --noconfirm google-chrome ttf-nerd-fonts-symbols nerd-fonts
 paru -S --noconfirm sddm qt6-virtualkeyboard qt6-multimedia-ffmpeg
 # Probably not needed, as it's not that stable compared to GNOME
-paru -S --noconfirm sddm-themes-git easyeffects
+paru -S --noconfirm sddm-silent-theme-git easyeffects
 paru -s --noconfirm wttrbar
 paru -S calf qjackctl lv2
 
@@ -10,4 +9,4 @@ paru -S calf qjackctl lv2
 sudo pacman -Syu gdm
 
 sudo pacman -S lsp-plugins
-sudo pacman -S jack gstreamer gst-plugins-base
+sudo pacman -S jack2 gst-plugins-base
