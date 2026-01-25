7d3b7f548a8210154b2ba7b6d62d66d8c6c7d0d5
limpid-kzonix <balyszyn@gmail.com>
2026-01-25 14:49:25 +0200

    feat(fedora+sway): add sway to fedora

---
+++ scripts/fedora/03-swaywm-packages.sh
@@ -0,0 +1,28 @@
+curl -fsSL https://vicinae.com/install.sh | bash
+
+# Install sway
+
+sudo dnf install sway \
+	dunst \
+	mako \
+	ncmpcpp \
+	pavucontrol \
+	ranger \
+	rofi \
+	fuzzel \
+	swaylock \
+	kitty \
+	waybar \
+	wl-clipboard \
+	mpc \
+	mpd \
+	mpv \
+	grim \
+	slurp \
+	zathura \
+	wob \
+	SwayNotificationCenter
+
+# Install sway-osd
+dnf copr enable erikreider/swayosd
+dnf install swayosd
