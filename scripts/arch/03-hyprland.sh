faa3ce602d35ad8f206e6a5cdc00d06383f09b57
limpid-kzonix <balyszyn@gmail.com>
2026-02-01 14:10:30 +0200

    feat: Configure monitor positions, establish persistent workspaces with updated Waybar icons, and expand screenshot keybindings.

--- scripts/arch/03-hyprland.sh
+++ scripts/arch/03-hyprland.sh
@@ -2,10 +2,12 @@
 
 sudo pacman -S nerd-fonts
 
-sudo pacman -Syu hyprlauncher hyprpaper hyprpicker hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprpwcenter hyprtoolkit hyprcursor  hyprland-guiutils
+sudo pacman -Syu hyprlauncher hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprpwcenter hyprtoolkit hyprcursor  hyprland-guiutils
+
+sudo pacman -S wf-recorder grim slurp jq wl-clipboard hyprpicker libnotify
 
 sudo pacman -S fuse2
 
 paru -Syu appimagelauncher
 
-paru -Syu dunst rofi swayosd
+paru -Syu dunst rofi swayosd hyprcap
