bb5b32e287a9b71287e6ed78efcc8e6fb6fabdf0
limpid-kzonix <balyszyn@gmail.com>
2026-05-09 18:24:20 +0300

    chore(flatpak): add new packages

---
+++ scripts/flatpak-obs-plugins.sh
@@ -0,0 +1,16 @@
+flatpak install flathub com.obsproject.Studio -y
+
+# OBS Studio Plugins
+flatpak install flathub com.obsproject.Studio.Plugin.DroidCam -y
+flatpak install flathub com.obsproject.Studio.Plugin.BackgroundRemoval -y
+flatpak install flathub com.obsproject.Studio.Plugin.Gstreamer -y
+flatpak install flathub com.obsproject.Studio.Plugin.GStreamerVaapi -y
+flatpak install flathub com.obsproject.Studio.Plugin.OBSPWVideo -y
+flatpak install flathub com.obsproject.Studio.Plugin.AitumMultistream -y
+flatpak install flathub com.obsproject.Studio.Plugin.SceneSwitcher -y
+flatpak install flathub com.obsproject.Studio.Plugin.WaylandHotkeys -y
+flatpak install flathub com.obsproject.Studio.Plugin.CompositeBlur -y
+flatpak install flathub com.obsproject.Studio.Plugin.AdvancedMasks -y
+flatpak install flathub com.obsproject.Studio.Plugin._3DEffect -y
+flatpak install flathub com.obsproject.Studio.Plugin.Shaderfilter -y
+flatpak install flathub com.obsproject.Studio.Plugin.TransitionTable -y
